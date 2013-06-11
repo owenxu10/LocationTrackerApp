2f29c95cf693f28a7a5fe3130dfb3710e8b9cfc1
bjrara <bjrara@sina.com>
2013-06-10 21:38:49 -0400

    alarm time selector merged

--- LocationTracker/AlarmSettingViewController.h
+++ LocationTracker/AlarmSettingViewController.h
@@ -8,7 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "DaySettingViewController.h"
-@interface AlarmSettingViewController : UITableViewController <AlarmSettingDelegate>
+@interface AlarmSettingViewController : UIViewController <AlarmSettingDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *lblText;
+@property (weak, nonatomic) IBOutlet UIButton *btnRepeat;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *btnSave;
+- (IBAction)savePressed:(id)sender;
+@property (weak, nonatomic) IBOutlet UIDatePicker *alarmTime;
 
 @end
